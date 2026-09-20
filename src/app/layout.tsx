5d050589fe6ac3400a6deddfda49ebcac8573df8
Dim64 <dim64@example.com>
2026-09-20 16:47:26 +0400

    Подключена Яндекс.Метрика

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,38 @@ export default function RootLayout({
         <Header />
         <div className="flex-1">{children}</div>
         <Footer />
+
+        {/* Knocket — онлайн-чат */}
         <Script
           src="https://trtc.io/knocket-sdk/sdk.js?identifier=f5fce5aead53166d8f&v=1789724698066"
           strategy="lazyOnload"
         />
+
+        {/* Яндекс.Метрика */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=112838071', 'ym');
+
+              ym(112838071, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+            `,
+          }}
+        />
+        <noscript>
+          <div>
+            {/* eslint-disable-next-line @next/next/no-img-element */}
+            <img
+              src="https://mc.yandex.ru/watch/112838071"
+              style={{ position: "absolute", left: "-9999px" }}
+              alt=""
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   );
